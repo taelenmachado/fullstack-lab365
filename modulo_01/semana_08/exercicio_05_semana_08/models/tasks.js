dc7ab9b9c7c674befb04c891a91a36ea23818700
Taelen Machado <taelen_machado@estudante.sesisenai.org.br>
2023-04-03 23:18:11 -0300

    Resposta do exercicio_05_semana_08

---
+++ modulo_01/semana_08/exercicio_05_semana_08/models/tasks.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require("sequelize");
+const connection = require("../db");
+
+const Tasks = connection.define("tasks", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Tasks;
